11b06105297d91e985c720ff1a099c9acdd573ea
Stefan Meier <stefan.r.meier@unil.ch>
2012-01-14 07:16:20 +0100

    separate test and prod data for db

--- php/res/calendar.sql
+++ php/res/calendar.sql
@@ -280,12 +280,6 @@ INSERT INTO `room_acls` (`room_id`, `read`, `write`, `overwrite`, `admin`, `deny
 (53, 'fbm-decanat-g', '', 'fbm-decanat-administration-secr-g', 'fbm-decanat-admin-g',''),
 (54, '*', 'fbm-licr-g', 'fbm-licr-admin-g', 'fbm-licr-admin-g','');
 
-
-INSERT INTO `rooms` (`room_id`, `building_id`, `room_category_id`, `local`, `name`, `manager`, `description`, `monitoring`, `maxEvents`) VALUES
-(55, 27, 4, 'test', 'test', '', '', 0, 1);
-INSERT INTO `room_acls` (`room_id`, `read`, `write`, `overwrite`, `admin`, `denyShibAttrib`) VALUES
-(55, '*', 'fbm-licr-admin-g', '', 'fbm-licr-admin-g','');
-
 -- --------------------------------------------------------
 
 --
