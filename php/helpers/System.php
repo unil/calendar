52b7a7a4758aef6a2686c5ca73ce2f412f787546
Stefan Meier <stefan.r.meier@unil.ch>
2012-01-25 19:47:19 +0100

    ACL and authentification fixed

--- php/helpers/System.php
+++ php/helpers/System.php
@@ -8,9 +8,8 @@
 class System {
 
 	private static function checkAttr($attr, $group, $aai_groups) {
-
 		$ret = false;
-		
+
 		if (in_array("*", $group)) {
 			$ret = true;
 		}
@@ -28,34 +27,28 @@ class System {
 	public static function auth($acl) {
 
 		$auth = array("read" => false, "write" => false, "overwrite" => false, "admin" => false);
-
+		$aai_groups = array();
+		$isDenied = false;
 
 		if (isset($_SERVER['HTTP_SHIB_EP_AFFILIATION']) && isset($_SERVER['HTTP_SHIB_CUSTOM_UNILMEMBEROF'])) {
 			##On récupère les groupes UNIL et on en fait un array
 			$aai_groups = explode(";", $_SERVER['HTTP_SHIB_CUSTOM_UNILMEMBEROF']);
 			$isDenied = in_array($_SERVER['HTTP_SHIB_EP_AFFILIATION'], $acl["denyShibAttrib"]);
-
-			if (!$auth["read"]) {
-				$auth["read"] = self::checkAttr("read", $acl["read"], $aai_groups) && !$isDenied;
-			}
-			
-			if (!$auth["overwrite"]) {
-				$auth["overwrite"] = self::checkAttr("overwrite", $acl["overwrite"], $aai_groups);
-				$auth["write"] = $auth["overwrite"];
-			}
-			if (!$auth["write"]) {
-				$auth["write"] = self::checkAttr("write", $acl["write"], $aai_groups);
-			}
-
-			if (!$auth["admin"]) {
-				$auth["admin"] = self::checkAttr("admin", $acl["admin"], $aai_groups);
-			}
-
 		}
+
+		$auth["read"] = !$isDenied && self::checkAttr("read", $acl["read"], $aai_groups);
+
+		$auth["overwrite"] = self::checkAttr("overwrite", $acl["overwrite"], $aai_groups);
+		$auth["write"] = $auth["overwrite"];
+
+		if (!$auth["write"]) {
+			$auth["write"] = self::checkAttr("write", $acl["write"], $aai_groups);
+		}
+
+		$auth["admin"] = self::checkAttr("admin", $acl["admin"], $aai_groups);
 
 		return $auth;
 	}
-
 }
 
 ?>
--- php/test/SystemTest.php
+++ php/test/SystemTest.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 class SystemTest extends PHPUnit_Framework_TestCase
 {	
 	protected $auth;
@@ -7,18 +7,8 @@ class SystemTest extends PHPUnit_Framework_TestCase
 	protected function setUp()
 	{
 		set_include_path("/Volumes/FILES/smeier/Sites/calendar/php");
-		include_once("../../config/init.php");
 		include_once("helpers/System.php");
-		include_once("model/class/Room.php");
-		include_once("model/RoomHandler.php");
 
-
-		/*$roomHandler = new RoomHandler();
-		$room = $roomHandler->getRooms(null, 19);
-		
-		$room = $room[0];
-		
-		print_r($room->getAcl());*/
 		$_SERVER['HTTP_SHIB_EP_AFFILIATION'] = "student";
 		$_SERVER['HTTP_SHIB_CUSTOM_UNILMEMBEROF'] = "fbm-dpt-g";
 		$this->auth = array(
@@ -30,71 +20,90 @@ class SystemTest extends PHPUnit_Framework_TestCase
 	}
 
 	public function testRead() {
-		echo "read test";
-		echo "test with *";
+		echo "\nread test\n";
+		echo "\ntest with *\n";
 		$this->auth["read"] = array("*");
 		$acl = System::auth($this->auth);
 		$this->assertTrue($acl["read"]);
 		
-		echo  "test with group";
+		echo  "\ntest with groupv";
 		$this->auth["read"] = array("fbm-dpt-g");
 		$acl = System::auth($this->auth);
 		$this->assertTrue($acl["read"]);
 		
-		echo "test more groups";
+		echo "\ntest more groups\n";
 		$this->auth["read"] = array("fbm-test", "toto", "fbm-dpt-g");
 		$acl = System::auth($this->auth);
 		$this->assertTrue($acl["read"]);
 
-		echo "test empty";
+		echo "\ntest empty\n";
 		$this->auth["read"] = array();
 		$acl = System::auth($this->auth);
 		$this->assertFalse($acl["read"]);
 		
-		echo "test with studen attrib";
+		echo "\ntest with studen attrib\n";
 		$this->auth["denyShibAttrib"] = array("student");
 		$this->auth["read"] = array("fbm-dpt-g");
 		$acl = System::auth($this->auth);
 		$this->assertFalse($acl["read"]);
 		
-		echo "with affiliation different from denied attribut";
+		echo "\nwith affiliation different from denied attribut\n";
 		$_SERVER['HTTP_SHIB_EP_AFFILIATION'] = "staff";
 		$acl = System::auth($this->auth);
 		$this->assertTrue($acl["read"]);
 	}
 	
 	public function testWrite() {
-		//test with *
+		echo "\nwrite\n";
+		echo "\ntest with *\n";
 		$this->auth["write"] = array("*");
 		$acl = System::auth($this->auth);
 		$this->assertTrue($acl["write"]);
 		
-		//test with group
+		echo "\ntest with group\n";
 		$this->auth["write"] = array("fbm-dpt-g");
 		$acl = System::auth($this->auth);
 		$this->assertTrue($acl["write"]);
 		
-		//test empty
+		echo "\ntest empty\n";
 		$this->auth["write"] = array();
 		$acl = System::auth($this->auth);
 		$this->assertFalse($acl["write"]);
 		
-		//test with studen attrib
+		echo "\ntest with studen attrib\n";
 		$this->auth["denyShibAttrib"] = array("student");
 		$acl = System::auth($this->auth);
 		$this->assertFalse($acl["write"]);
 				
-		//overwrite is different from write
+		echo "\noverwrite is different from write\n";
 		$this->auth["overwrite"] = array("fbm-dpt-test-g");
 		$this->auth["write"] = array("fbm-dpt-g");
 		$acl = System::auth($this->auth);
 		$this->assertTrue($acl["write"]);
 		
-		//overwrite is specified but not write (inherit)
+		echo "\noverwrite is specified but not write (inherit)\n";
 		$this->auth["overwrite"] = array("fbm-dpt-g");
 		$this->auth["write"] = array();
 		$acl = System::auth($this->auth);
 		$this->assertTrue($acl["write"]);
 	}	
+	
+	public function testRoom() {	
+		include_once("../../config/init.php");
+		include_once("model/class/Room.php");
+		include_once("model/RoomHandler.php");
+		$_SERVER['HTTP_SHIB_CUSTOM_UNILMEMBEROF'] = "fbm-decanat-administration-secr-g";
+		$_SERVER['HTTP_SHIB_EP_AFFILIATION'] = "staff";
+		$roomHandler = new RoomHandler();
+		$room = $roomHandler->getRooms(null, 53);
+		
+		$room = $room[0];
+		
+		$acl = $room->getAcl();
+		$auth = System::auth($acl);
+		$this->assertTrue($auth["write"]);
+		//var_dump($acl);
+		//var_dump($auth);
+	}
 }
 ?>
